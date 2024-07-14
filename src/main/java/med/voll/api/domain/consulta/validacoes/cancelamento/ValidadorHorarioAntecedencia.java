4dad8c2bf5888ccc3421f8c657df4363eaadc38c
Nordesith <rutilohahn12@gmail.com>
2024-07-14 14:17:54 -0300

    Adicionados os testes unitarios para controllers e repositories

--- src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
@@ -1,8 +1,7 @@
 package med.voll.api.domain.consulta.validacoes.cancelamento;
 
-import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidacaoException;
 import med.voll.api.domain.consulta.ConsultaRepository;
-import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -2,7 +2,7 @@ package med.voll.api.infra.exception;
 
 
 import jakarta.persistence.EntityNotFoundException;
-import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidacaoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
