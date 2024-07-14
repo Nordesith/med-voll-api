7690053267c93ab5491e39851206a8388e85259f
Nordesith <rutilohahn12@gmail.com>
2024-07-14 19:56:51 -0300

    Organização de código aprimorada

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -2,7 +2,6 @@ package med.voll.api.infra.exception;
 
 
 import jakarta.persistence.EntityNotFoundException;
-import med.voll.api.domain.consulta.validacoes.ValidacaoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
